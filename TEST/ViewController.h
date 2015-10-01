e110d6ed66fbf0fee70bdb6407b666885bc1f7db
Blue Orca 05 <blueorca@Blues-iMac.local>
2015-10-01 05:42:38 -0700

    Initial Commit

---
+++ TEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST
+//
+//  Created by Blue Orca 05 on 01/10/2015.
+//  Copyright (c) 2015 BlueOrca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TEST/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TEST
+//
+//  Created by Blue Orca 05 on 01/10/2015.
+//  Copyright (c) 2015 BlueOrca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
